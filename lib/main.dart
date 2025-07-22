ad497b48aa84b31288651a233aa5c64ab869e4ea
Vitor-Ribe <vitorribeirodev@gmail.com>
2025-07-21 21:01:31 -0300

    Adição da bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TI Maromba',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -30,9 +32,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
+    final items = <Widget> [
+      Icon(Icons.home, size: 30, color: Colors.white),
+      Icon(Icons.monitor_heart, size: 30, color: Colors.white),
+      Icon(Icons.water_drop, size: 30, color: Colors.white),
+      Icon(Icons.percent, size: 30, color: Colors.white),
+      Icon(Icons.person, size: 30, color: Colors.white)
+    ];
+
     return Scaffold(
       appBar: AppBar(
        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -42,7 +53,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
             Text(
               'Hello World!',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -50,6 +60,16 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+            bottomNavigationBar: CurvedNavigationBar(
+              backgroundColor: Colors.transparent,
+              animationCurve: Curves.easeInOut,
+              color: Colors.blue.shade700,
+              animationDuration: Duration(milliseconds: 300),
+              height: 75,
+              items: items,
+              index: index,
+              onTap: (index) => setState(() => this.index = index),
+            ),
     );
   }
 }
